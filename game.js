463381496e4539301aa22363d399b434a4c17ff2
Sam Clearman <sam@samclearman.com>
2013-05-23 01:22:59 -0400

    basic event loop

--- game.js
+++ game.js
@@ -1,53 +1,104 @@
-var SHADOW_SIZE = 25;
+var SHADOW_SIZE = 20;
+var BLOCK_SIZE = 100;
 var SHADOW_COLOR = "rgb(128,128,128)";
+var RUNNING = true;
 
 function addPts(p,q) {
 	return({x: p.x + q.x, y: p.y + q.y})
 }
-var entities = [];
+
+
 
 function game() {
+	
 	var canvas = document.getElementById('game');
-	entities.push(new block())
-	if (canvas.getContext){
-		var ctx = canvas.getContext("2d");
+	var ctx = canvas.getContext("2d");
+	var lastFrame = new Date().getTime();
+	
+	var entities = [];
+	entities.push({
+		drawOn: function(ct) {
+			ct.fillStyle = "white";
+			ct.fillRect(0,0,390,400);
+			// ct.fillStyle = "grey";
+			// ct.fillRect(390,0,20,400);
+			ct.fillStyle = "black";
+			ct.fillRect(410,0,400,400);
+		},
+		update: function(delta) { }
+	})
+	entities.push(new block(410,320))
+	entities.push(new block(310,100))
+	entities.push(new block(390,220,undefined,"white","NE"))
+	
+	function update(delta) {
+		
+		for(var i=0; i<entities.length; i++) {
+			entities[i].update(delta);
+		}
+		
+		for(var i = entities.length - 1; i >= 0; i--) {
+			if (entities[i].destroyed) {
+				entities.splice(i,1);
+			}
+		}
+				
+	}
+
+	function redraw() {
 		for(var i = 0; i < entities.length; i++) {
 			entities[i].drawOn(ctx);
 		}
 	}
-}
 
-entities.push({
-	drawOn: function(ctx) {
-		ctx.fillStyle = "white";
-		ctx.fillRect(0,0,400,400);
-		ctx.fillStyle = "black";
-		ctx.fillRect(400,0,400,400);
+	function loop(){
+		now = new Date().getTime();
+		delta = (now - lastFrame) / 1000;
+		update(delta);
+		redraw();
+		lastFrame = now;
 	}
-})
-
-function block() {
-	this.x = 300;
-	this.y = 100;
-	this.width = 200;
-	this.height = 200;
-	this.color = "black";
-	this.shadowDirection = "NE"
+	
+	var loopId = setInterval(loop, 10);
+	
 }
+
+
+function grid() {
+	this.state = [[1,1,1,1,0,0,0,0],
+				  [1,1,1,1,0,0,0,0],
+				  [1,1,1,1,0,0,0,0],
+				  [1,1,1,1,0,0,0,0]];
+}
+
+grid.prototype.update = function() {}
+
+grid.prototype.drawOn = function(ctx) {
+	
+}
+
+function block(x,y,velocity,color, shadowDirection) {
+	this.x = x || 0;
+	this.y = y || 0;
+	this.width = BLOCK_SIZE;
+	this.height = BLOCK_SIZE;
+	this.color = color || "black";
+	this.shadowDirection = shadowDirection || "SW";
+	this.velocity = velocity || {x: 0, y: 0};
+};
 
 block.prototype.NW = function() {
 	return {x: this.x, y: this.y};
-}
-
+};
 block.prototype.NE = function() {
 	return {x: this.x + this.width, y: this.y};
-}
+};
 block.prototype.SW = function() {
 	return {x: this.x, y: this.y + this.height};
-}
+};
 block.prototype.SE = function() {
 	return {x: this.x + this.width, y: this.y + this.height};
-}
+};
 
 block.prototype.shadowPoints = function() {
 	var points = [];
@@ -66,13 +117,17 @@ block.prototype.shadowPoints = function() {
 	}
 	points.push(this[route[route.length - 1]]());
 	return points;
-}
+};
+
+block.prototype.update = function(delta) {
+	this.x += delta * this.velocity.x
+	this.y += delta * this.velocity.y
+};
 
 block.prototype.drawOn = function(ctx) {
 	ctx.fillStyle = SHADOW_COLOR;
 	ctx.beginPath();
 	var pts = this.shadowPoints();
-	console.log(pts);
 	ctx.moveTo(pts[0].x, pts[0].y);
 	for(var i=1; i<pts.length; i++) {
 		ctx.lineTo(pts[i].x, pts[i].y);
@@ -80,4 +135,4 @@ block.prototype.drawOn = function(ctx) {
 	ctx.fill();
 	ctx.fillStyle = this.color;
 	ctx.fillRect(this.x, this.y, this.width, this.height);
-}
+};
